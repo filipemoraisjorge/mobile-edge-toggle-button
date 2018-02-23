88976da2f719ca15531b207ace09a977376d573f
filipemoraisjorge <filipemoraisjorge@gmail.com>
2018-02-23 21:36:59 +0100

    add shopify/draggable

--- src/components/toggleEdge.jsx
+++ src/components/toggleEdge.jsx
@@ -1,4 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Draggable} from '@shopify/draggable';
 import './toogleEdge.css';
 
-export default (props) => (<div className="toggleEdge">{props.value}</div>);
+class ToogleEdge extends Component {
+
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+  };
+
+  componentDidMount() {
+    const containers = document.querySelectorAll('.toggleEdge--container');
+    if (containers.length === 0) {
+      return false;
+    }
+
+    const draggable = new Draggable(containers, {
+      draggable: '.toggleEdge--button',
+      delay: 0,
+    });
+
+    draggable.on('drag:start', (evt) => {
+      console.log('drag:start', evt);
+    });
+
+    draggable.on('drag:stop', (evt) => {
+      console.log('drag:stop', evt);
+    });
+  }
+
+  render() {
+    return (
+      <div className="toggleEdge--container">
+        <div className="toggleEdge--button" >{this.props.value}</div>
+      </div>
+    );
+  };
+
+}
+
+export default ToogleEdge;
